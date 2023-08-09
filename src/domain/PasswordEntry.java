3c7e8089f46bc46d0c310d861576aceee1655f25
Manav Singh <116850335+M4N4VS1NGH@users.noreply.github.com>
2023-08-08 23:04:11 -0400

    Update PasswordEntry.java

--- src/domain/PasswordEntry.java
+++ src/domain/PasswordEntry.java
@@ -4,19 +4,23 @@ public class PasswordEntry {
     private String username;
     private String password;
 
+    // Initialize username and password
     public PasswordEntry(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    // Return username
     public String getUsername() {
         return username;
     }
 
+    // Return password
     public String getPassword() {
         return password;
     }
 
+    // Set the object's password attribute to what is given
     public void setPassword(String password) {
         this.password = password;
     }

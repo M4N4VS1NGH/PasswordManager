ad0e84559f6f285408be4545240114efae9ad9a7
Manav Singh <116850335+M4N4VS1NGH@users.noreply.github.com>
2023-08-08 23:03:10 -0400

    Update UserNotificationObserver.java

--- src/application/UserNotificationObserver.java
+++ src/application/UserNotificationObserver.java
@@ -1,5 +1,6 @@
 package application;
 
+// Sending out an update to the user when an action has occurred 
 public class UserNotificationObserver implements Observer {
     @Override
     public void update(String website, String action) {
